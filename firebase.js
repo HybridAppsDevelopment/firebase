32b6c48a44a6b1028923ce4b24d46deee9b37281
Dr Andrew Bingham <a.p.bingham@tees.ac.uk>
2019-02-25 13:30:15 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+(function(){
+
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "yourAppKey",
+      authDomain: "yourDomain",
+      databaseURL: "https://yourdb.com",
+      storageBucket: "storageURL",
+      messagingSenderId: "yourID"
+    };
+    firebase.initializeApp(config);
+
+    
+}());
